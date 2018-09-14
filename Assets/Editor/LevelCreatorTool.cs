9cc1f02e322d5f15312f495982baf9a5d7093496
Revinz <revinz1414@gmail.com>
2018-09-14 17:34:25 +0200

    LevelEditor Window Openable

---
+++ Assets/Editor/LevelCreatorTool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LevelCreatorTool : EditorWindow {
+
+    [MenuItem("Tools/Level Editor")]
+    private static void OpenLevelEditor()
+    {
+        EditorWindow.GetWindow(typeof(LevelCreatorTool));
+    }
+
+    void OnGUI()
+    {
+        // The actual window code goes here
+    }
+}
