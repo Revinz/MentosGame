511d498f13f5ece2433d5bec4147e68629f63fc2
Revinz <revinz1414@gmail.com>
2018-09-14 15:05:54 +0200

    Simple board Generation -- Working

--- Assets/BoardGenerator.cs
+++ Assets/BoardGenerator.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class BoardGenerator : MonoBehaviour {
 
-    public int size = 5;
+    public int sizeX = 8;
+    public int sizeY = 8;
 
 	// Use this for initialization
 	void Start () {
@@ -15,12 +16,15 @@ public class BoardGenerator : MonoBehaviour {
     void GenerateBoard()
     {
         GameObject tile = (GameObject)Resources.Load("BoardTilePrototype");
+        float ObjectScale = tile.transform.localScale.x;
 
-        for (int x = 0; x < size; x++)
+        for (int x = 0; x < sizeX; x++)
         {
-            for (int y = 0; y < size; y++)
+            for (int y = 0; y < sizeY; y++)
             {
-                Instantiate(tile, new Vector3(1 * x, 1 * y, 0), Quaternion.identity);
+                float offsetX = (ObjectScale * sizeX) / 2;
+                float offsetY = (ObjectScale * sizeY) / 2;
+                Instantiate(tile, new Vector3(ObjectScale * x - offsetX, 0 , ObjectScale * y - offsetY), Quaternion.identity);
             }
         }
     }
